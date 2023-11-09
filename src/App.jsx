6b65004bc8286d671dca3bfe8675bbd3f0852cb9
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-09 10:32:14 -0300

    config backend deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import io from 'socket.io-client';
 import Chat from './Chat';
-import { Container, Card, Icon, Form, Button, Header} from 'semantic-ui-react';
+import { Container, Card, Form, Button, Header} from 'semantic-ui-react';
 import { Info } from './consts'
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://romin-chats-backend.vercel.app/");
 
 function App() {
   const [username, setUsername] = useState("");
