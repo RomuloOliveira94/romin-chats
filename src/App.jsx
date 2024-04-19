25c067efb576f61f67b257d15c3d8429bf7a83c6
Rômulo Augusto <romuloffall@gmail.com>
2024-04-18 21:52:04 -0300

    #fix chat count

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,70 @@
-import { useState } from 'react';
-import io from 'socket.io-client';
-import Chat from './Chat';
-import { Container, Card, Form, Button, Header} from 'semantic-ui-react';
-import { Info } from './consts'
-const socket = io("http://localhost:5000");
+import { useState } from "react";
+import io from "socket.io-client";
+import Chat from "./Chat";
+import { Container, Card, Form, Button, Header } from "semantic-ui-react";
+import { Info } from "./consts";
+const socket = io("https://romin-chats-backend.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
+  const [usersQuantity, setUsersQuantity] = useState("");
+
 
   const joinRoom = async () => {
     if (room != "" && username != "") {
-      const info = new Info(`${username} entrou na sala`, room, true)
-      socket.emit("join_room", info)
+      const info = new Info(`${username} entrou na sala`, room, true);
+      socket.emit("join_room", info);
       setShowChat(true);
+      console.log('entrou')
     }
-  }
-
+  };
 
   return (
-    <Container style={{margin: "20px"}} >
-      <Header as='h1' textAlign="center">Reservoir Dogs +52</Header>
+    <Container style={{ margin: "20px" }}>
+      <Header as="h1" textAlign="center">
+        Romin Chats
+      </Header>
+      <Header as="h2">Chat em tempo real, entre em uma sala.</Header>
       {!showChat ? (
-      <Card fluid>
-        <Card.Content header='Entrar no Chat' />
+        <Card fluid>
+          <Card.Content header="Entrar no Chat" />
           <Card.Content>
             <Form>
               <Form.Field>
-                <label>Nome</label>
-                <input 
-                placeholder='Nome' 
-                onChange={(e) => setUsername(e.target.value)}
+               <label>Nome</label>
+                <input
+                  placeholder="Nome"
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </Form.Field>
               <Form.Field>
                 <label>Sala</label>
                 <input
-                type="number" 
-                placeholder='Número'
-                onChange={(e) => setRoom(e.target.value)}
+                  type="number"
+                  placeholder="Número"
+                  onChange={(e) => setRoom(e.target.value)}
                 />
               </Form.Field>
-              <Button onClick={joinRoom} color="blue" fluid>Entrar</Button>
-          </Form>
-        </Card.Content>
-      </Card>
+              <Button onClick={joinRoom} color="blue" fluid>
+                Entrar
+              </Button>
+            </Form>
+          </Card.Content>
+        </Card>
       ) : (
-      <Chat socket={socket} 
-      username={username} 
-      room={room} 
-      setShowChat={setShowChat} 
-      />
+        <Chat
+          socket={socket}
+          username={username}
+          room={room}
+          setShowChat={setShowChat}
+          usersQuantity={usersQuantity}
+          setUsersQuantity={setUsersQuantity}
+        />
       )}
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,100 +1,114 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Card, Icon, Form, Input, Message, Divider, Button } from 'semantic-ui-react'
-import ScrollToBottom from "react-scroll-to-bottom"
-import { Info } from './consts'
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import {
+  Container,
+  Card,
+  Icon,
+  Form,
+  Input,
+  Message,
+  Divider,
+  Button,
+} from "semantic-ui-react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import { Info } from "./consts";
 
-const Chat = ({socket, username, room, setShowChat}) => {
+const Chat = ({
+  socket,
+  username,
+  room,
+  setShowChat,
+  usersQuantity,
+  setUsersQuantity,
+}) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [messageList, setMessageList] = useState([]);
-    const [usersQuantity, setUsersQuantity] = useState("");
-
-    const sendMessage = async () => {
-        if(username && currentMessage){
-            const info = new Info(currentMessage, room, true, username)
-            await socket.emit("send_message", info);
-            setMessageList((messages) => [...messages, info])
-            setCurrentMessage("")
-        }
+  const sendMessage = async () => {
+    if (username && currentMessage) {
+      const info = new Info(currentMessage, room, true, username);
+      await socket.emit("send_message", info);
+      setMessageList((messages) => [...messages, info]);
+      setCurrentMessage("");
     }
+  };
 
-    const disconnectFromRoom = async () => {
-        const info = new Info(`${username} saiu da sala`, room, false)
-        await socket.emit('leaveRoom', info);
-        setShowChat(false);
-    }
-
-    useEffect(() => {
-        const handleMessage = (data) =>{
-            setMessageList((messages) => [...messages, data])
-        } 
-        const handleQuantityOfUsers = (data) => {
-            setUsersQuantity(data)
-        }
-        socket.on("receive_message", handleMessage);
-        socket.on("userQuantity", handleQuantityOfUsers);
-        return () => {
-            socket.off("receive_message", handleMessage);
-            socket.off("userQuantity", handleQuantityOfUsers);
-        }
-    },[]);
+  const disconnectFromRoom = async () => {
+    const info = new Info(`${username} saiu da sala`, room, false);
+    await socket.emit("leaveRoom", info);
+    setShowChat(false);
+  };
 
 
-    return (
-        <Container>
-            <Card fluid>
-                <Card.Content header={`Sala ${room}`} />
-                    <ScrollToBottom>                     
-                        <Card.Content style={{height: "500px", padding: "5px"}}>
-                            {messageList.map((m,i) => {                              
-                            const verifyAuthor = username === m.author;
-                            return <span key={i}>
-                                <Message
-                                style={{textAlign: verifyAuthor ? 'right' : 'left'}}
-                                warning={!m.userOn}
-                                success={verifyAuthor}
-                                info={!verifyAuthor}
-                                >
-                                <Message.Header key={i}>{m.message}</Message.Header>
-                                    <p><strong>{m.author ? m.author : ""}</strong><i> ás {m.time}h</i></p>
-                                </Message>
-                                <Divider/>
-                            </span> 
-                            })}
-                        </Card.Content>
-                    </ScrollToBottom>
-                <Card.Content extra>
-                    <Form>
-                        <Form.Field>
-                            <Input 
-                            type="text" 
-                            placeholder='Mensagem...' 
-                            onChange={e => setCurrentMessage(e.target.value)}
-                            onKeyPress={(e) => e.key === "enter" ? sendMessage : "" }
-                            action={{
-                                color: "green",
-                                icon: "send",
-                                labelPosition: "right",
-                                content: "Enviar",
-                                onClick: sendMessage, 
-                            }}
-                            value={currentMessage}
-                            />
-                        </Form.Field>
-                    </Form>
-                </Card.Content>
-                <Card.Content>
-                <Icon name='user'/>{ usersQuantity }
-                </Card.Content>
-            </Card>
-            <Button 
-            color={"red"}
-            floated={"right"}
-            onClick={disconnectFromRoom}
-            >Desconectar da Sala
-            </Button>
-        </Container>
-    )
-}
- 
+  useEffect(() => {
+    const handleMessage = (data) => {
+      setMessageList((messages) => [...messages, data]);
+    };
+    const handleQuantityOfUsers = (data) => {
+      setUsersQuantity(data);
+    };
+    socket.on("receive_message", handleMessage);
+    socket.on("userQuantity", handleQuantityOfUsers);
+  }, [socket, setUsersQuantity]);
+
+  return (
+    <Container>
+      <Card fluid>
+        <Card.Content header={`Sala ${room}`} />
+        <ScrollToBottom>
+          <Card.Content style={{ height: "500px", padding: "5px" }}>
+            {messageList.map((m, i) => {
+              const verifyAuthor = username === m.author;
+              return (
+                <span key={i}>
+                  <Message
+                    style={{ textAlign: verifyAuthor ? "right" : "left" }}
+                    warning={!m.userOn}
+                    success={verifyAuthor}
+                    info={!verifyAuthor}
+                  >
+                    <Message.Header key={i}>{m.message}</Message.Header>
+                    <p>
+                      <strong>{m.author ? m.author : ""}</strong>
+                      <i> ás {m.time}h</i>
+                    </p>
+                  </Message>
+                  <Divider />
+                </span>
+              );
+            })}
+          </Card.Content>
+        </ScrollToBottom>
+        <Card.Content extra>
+          <Form>
+            <Form.Field>
+              <Input
+                type="text"
+                placeholder="Mensagem..."
+                onChange={(e) => setCurrentMessage(e.target.value)}
+                onKeyPress={(e) => (e.key === "enter" ? sendMessage : "")}
+                action={{
+                  color: "green",
+                  icon: "send",
+                  labelPosition: "right",
+                  content: "Enviar",
+                  onClick: sendMessage,
+                }}
+                value={currentMessage}
+              />
+            </Form.Field>
+          </Form>
+        </Card.Content>
+        <Card.Content>
+          <Icon name="user" />
+          {usersQuantity}
+        </Card.Content>
+      </Card>
+      <Button color={"red"} floated={"right"} onClick={disconnectFromRoom}>
+        Desconectar da Sala
+      </Button>
+    </Container>
+  );
+};
+
 export default Chat;
